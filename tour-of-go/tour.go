ef2f5c842cdee8fdf9f466fbb2b109bf2b6fbcd0
Jorge Celaya <jcelaya775@gmail.com>
2024-02-17 18:37:35 -0600

    Create Image type

--- tour-of-go/tour.go
+++ tour-of-go/tour.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"io"
 	"math"
 	"strings"
@@ -183,4 +185,18 @@ func isUpperCase(char byte) bool {
 
 func isLowerCase(char byte) bool {
 	return char >= 'a' && char <= 'z'
+}
+
+type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 100, 100)
+}
+
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x), uint8(y), 255, 255}
 }
